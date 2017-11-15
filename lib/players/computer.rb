a5b2667b8f60cd2b75109eecdc790b2f8452c9f9
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 15:21:14 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,6 @@
 module Players
   class Computer < Player
+    # Inherits initialize from the Player class.
 
     WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
